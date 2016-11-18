3189e6327c0983d4470dacab9092f362e79b58e2
dungtranmicroad <dung_tran@microad.vn>
2016-11-18 14:13:00 +0700

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -143,15 +143,7 @@ after_initialize do
       image = Upload.get_from_url(object.image_url) rescue false
       return ListHelper.create_thumbnails(object.id, image, object.image_url)
     end
-def image_html
-          src = data[:image] || data[:thumbnail_url]
-          return if Onebox::Helpers.blank?(src)
 
-          alt    = data[:description]  || data[:title]
-          width  = data[:image_width]  || data[:thumbnail_width]
-          height = data[:image_height] || data[:thumbnail_height]
-          "<img src='#{src}' alt='#{alt}' width='#{width}' height='#{height}'>"
-end
     def topic_post_actions
       return [] if !scope.current_user
       PostAction.where(post_id: topic_post_id, user_id: scope.current_user.id)
